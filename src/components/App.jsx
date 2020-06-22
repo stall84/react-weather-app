0b2743eb811f6728496b16b54fbf6b7177ef841e
Michael Stallings <stall84@gmail.com>
2020-06-22 12:13:45 -0400

    trimmed up App.jsx, removed unecessary router components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,6 @@
 import React, { Component } from 'react';
-import WeatherMain from './WeatherMain';
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link
-} from 'react-router-dom';
+
+
 
 import '../styles/App.css';
 
@@ -26,30 +21,18 @@ cityInput = React.createRef()
 
   render() {
     
-      return (  
-        <React.Fragment>
-          <Switch>
-
-            <Route exact path="/">
+      return (       
               <div className="container searchForm">
-                <h2>Enter a city, state, country<br/>for local conditions</h2>
-                <form className="citySearchForm" onSubmit={this.goToCity}>
-                  <label htmlFor="citySearch">City Search</label>
-                  <input ref={this.cityInput} type="text" id="citySearch" name="citySearch" placeholder="e.g. Atlanta,GA,USA"/>
-                  <input type="submit" />
-                </form>
+
+                  <h2>Enter a city, state, country<br/>for local conditions</h2>
+
+                    <form className="citySearchForm" onSubmit={this.goToCity}>
+                      <label htmlFor="citySearch">City Search</label>
+                      <input ref={this.cityInput} type="text" id="citySearch" name="citySearch" placeholder="e.g. Atlanta,GA,USA"/>
+                      <input type="submit" />
+                    </form>
 
               </div>
-            </Route>
-
-            <Route path="/curr-cond">
-                <div class="mainWeatherDiv">
-                <WeatherMain />
-                </div>
-            </Route>
-
-          </Switch>
-        </React.Fragment>
     )
   }
 
